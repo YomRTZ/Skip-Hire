fb348761da8bf9c8c4c6ea09d04b431355969336
HP <rediet754@gmail.com>
2025-06-06 23:59:47 -0700

    update

--- src/commons/Card.jsx
+++ src/commons/Card.jsx
@@ -1,6 +1,6 @@
-const Card = ({ children }) => (
-  <div className="bg-brand-light shadow-lg rounded-lg overflow-hidden w-full transition-all duration-300 hover:shadow-xl h-80">
-    <div className="pb-6 sm:pb-6  md:pb-8  lg:pb-8 xl:pb-8">
+const Card = ({ children,className, ...props }) => (
+  <div className={`bg-brand-light shadow-lg rounded-lg overflow-hidden w-full transition-all duration-300 hover:shadow-xl h-80 ${className}`}{...props}>
+    <div className="{pb-6 sm:pb-6  md:pb-8  lg:pb-8 xl:pb-8}">
       {children}
     </div>
   </div>
--- src/features/components/SkipCard.jsx
+++ src/features/components/SkipCard.jsx
@@ -1,33 +1,52 @@
+import React, { useState } from "react";
 import Card from "../../commons/Card";
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
-
-const SkipCard = ({ skip }) => (
-  <Card>
-    <div className="relative">
-      <div className="absolute top-2 right-2 bg-brand text-brand-color text-xs font-semibold rounded px-2 py-1 z-10 shadow">
-        {skip.size} Yard Skip
+import { ArrowRightIcon, CheckIcon } from "@heroicons/react/24/outline";
+function SkipCard({ skip }) {
+  const [selected, setSelected] = useState(false);
+  return (
+    <Card className="hover:shadow-lg  hover:border-2 hover:scale-[1.02] rounded transition duration-200 ease-in-out hover:border-amber-400">
+      <div className="relative">
+        <div className="absolute top-2 right-2 bg-brand text-brand-color text-xs font-semibold rounded px-2 py-1 z-10 shadow">
+          {skip.size} Yard Skip
+        </div>
+        <img
+          src={`https://yozbrydxdlcxghkphhtq.supabase.co/storage/v1/object/public/skips/skip-sizes/${skip.size}-yarder-skip.jpg`}
+          alt={`Skip Size ${skip.size}`}
+          className="w-full h-48 object-cover rounded"
+        />
       </div>
-      <img
-        src={`https://yozbrydxdlcxghkphhtq.supabase.co/storage/v1/object/public/skips/skip-sizes/${skip.size}-yarder-skip.jpg`}
-        alt={`Skip Size ${skip.size}`}
-        className="w-full h-48 object-cover rounded"
-      />
-    </div>
-    <div className="px-2 mt-4">
-      <p className="text-brand text-xl font-bold font-heading">
-        {skip.hire_period_days} hire period
-      </p>
-      <p className="text-brand text-sm font-semibold font-heading">
-        £{skip.getTotalPrice().toFixed(2)}
-      </p>
-    </div>
-    <div className="mt-3 flex justify-center">
-      <button className="flex items-center gap-2 bg-brand text-brand-color px-4 py-2  rounded hover:bg-brand-dark transition">
-       <div className="circle"><ArrowRightIcon className="w-4 h-4 text-brand-color" /></div>
-        <span>Rent This Skip</span>
-      </button>
-    </div>
-  </Card>
-);
+      <div className="px-2 mt-4">
+        <p className="text-brand text-xl font-bold font-heading">
+          {skip.hire_period_days} hire period
+        </p>
+        <p className="text-brand text-sm font-semibold font-heading">
+          £{skip.getTotalPrice().toFixed(2)}
+        </p>
+      </div>
+      <div className="mt-3 flex justify-center">
+        <button
+          className={`flex items-center gap-2 px-4 py-2 rounded transition 
+    ${
+      selected
+        ? "bg-brand-color text-brand"
+        : "bg-brand text-brand-color hover:bg-brand-dark"
+    }`}
+          onClick={() => setSelected(!selected)}
+        >
+          <div className="circle">
+            {selected ? (
+              <CheckIcon className="w-4 h-4 text-brand" />
+            ) : (
+              <ArrowRightIcon className="w-4 h-4 text-brand-color" />
+            )}
+          </div>
+          <span className="transition-colors duration-200">
+            {selected ? "Selected" : "Rent This Skip"}
+          </span>
+        </button>
+      </div>
+    </Card>
+  );
+}
 
 export default SkipCard;
